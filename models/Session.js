51bf65fd88b6204b79cd411bf3a6be060010f2a6
Guree sandhu <113997269+guri5405@users.noreply.github.com>
2026-06-12 13:01:32 +0530

    Create Session model with schema definition

---
+++ models/Session.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const sessionSchema = new mongoose.Schema(
+  {
+  user: { type: mongoose.Schema.Types.ObjectId,
+  ref: 'User', required: true },
+
+  refreshToken: { type: String, required: true },
+
+  userAgent: { type: String },
+  
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Session', sessionSchema);
