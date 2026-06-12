7f7375c719c599ac86322f97b5e3ac054930d87c
Guree sandhu <113997269+guri5405@users.noreply.github.com>
2026-06-12 12:56:25 +0530

    Create db.js

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connectDB = async (uri) => {
+  if (!uri) throw new Error('MONGO_URI is required');
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log('MongoDB connected');
+};
+
+module.exports = connectDB;
